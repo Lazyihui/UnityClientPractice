93bacf16dc0d266deb85d9d788c66bf0f83ce1c6
Lazyihui <chengbaozi6@qq.com>
2025-06-15 16:25:15 +0800

    <feature>add:增加panel的InputFiled

--- Assets/Src_Runtime/AppUI/Panel/Panel_Login.cs
+++ Assets/Src_Runtime/AppUI/Panel/Panel_Login.cs
@@ -1,14 +1,15 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
-
+// using TMPro;
 
 namespace Game_Client {
-
 
     public class Panel_Login : MonoBehaviour {
         [SerializeField] Button button_Login;
         public Action OnLoginClickHandle;
+
+        // [SerializeField] TextMeshProGUI text_Title;
 
 
         public void Ctor() {
